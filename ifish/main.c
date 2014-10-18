77750bce182950a055518efe54f6c21cebad88f4
BluABK <abk@blucoders.net>
2014-10-18 18:49:58 +0200

    Made a wannabe-shell of sorts

--- ifish/main.c
+++ ifish/main.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
-#include <errno.h> // stderr
+#include <stdlib.h>	// getenv & friends
+#include <errno.h>	// stderr
+#include <stdbool.h>	// boolean supporti
+#include <unistd.h>	// UNIX
+
+// Define some constants
+#define MAX_LENGTH 120
 
 #define DEBUG
 
+// Set global variables
+char *shell = "ifish";
+bool run = true;
+int cnt = 0;
 
-int main(void) {
+void prompt(int cmd_cnt) {
+	//return getenv("USER") + "@" + "ifish " + cmd_cnt + ":" + getenv("PATH") +"$ ";
+	printf( "%s@%s %d:%s> ", getenv("USER"), shell, cmd_cnt, getenv("PWD") );
+}
 
+// Program exit handler
+void quit() {
+	printf("\n");
+	run = false;
+}
+
+int main(int argc, char *argv[]) {
 	#ifdef DEBUG
-	//fprintf(stderr, "Running in debug mode\n\n");
-	printf("Running in debug mode\n\n");
+		printf("Running in debug mode\n\n");
 	#endif
-	
-	printf("I ran!\n");
+	char line[MAX_LENGTH];
+
+	// Program main loop
+	while (run) {
+	//	prompt = set_prompt(cnt++);
+	//	printf(prompt);
+		prompt(cnt++);
+		if (!fgets(line, MAX_LENGTH, stdin)) run = false;
+		system(line);
+	}	
 
 	return 0;
 }
