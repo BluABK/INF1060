b15d5ff89bf33aca374641c82e7ac20dec7b6c93
BluABK <abk@blucoders.net>
2014-10-21 14:45:35 +0200

    OMG IT COMPILED ! \o/ aka moving all command execution out of main and into runc()

--- ifish/main.c
+++ ifish/main.c
@@ -89,102 +89,94 @@ void print_debug(char *sh, char **param) {
 }
 #endif
 
-int main(void) {
-	#ifdef DEBUG
-		printf("Running in debug mode\n");
-	#endif
-
-	char line[MAX_LENGTH];
+void runc(char *line, bool run) {
 	char *param[21];
 	char * token;
 	char *path;
 	bool background = false;
 	int i, job;
-
-	// Program main loop
-	while (!feof(stdin)) {
-		prompt();
-		if (!fgets(line, MAX_LENGTH, stdin)) break;
-
-		// Parse and execute command
-		background = false;
-		for (i = strlen(line)-1; i >= 0; i--) {
-			if (isspace(line[i]))
-				line[i] = 0;
-			else if (line[i] == '&') {
-			       	background=true;
-				line[i] = 0;
-			}
-			else break;
+	
+	// Parse and execute command
+	background = false;
+	for (i = strlen(line)-1; i >= 0; i--) {
+		if (isspace(line[i]))
+			line[i] = 0;
+		else if (line[i] == '&') {
+		       	background=true;
+			line[i] = 0;
 		}
-		#ifdef DEBUG
-		fprintf(stderr, "Background? %s\n", background ? "yes" : "no");
-		#endif
-
-		// Get the first token (command)
-		token = strtok(line, DELIMITERS);
-
-		// Iterate throgh the rest (parameters)
-		for (i = 0; token != NULL && i < 20;i++) {
-			#ifdef DEBUG
-				printf("%s --> param[%d]\n", token, i);
-			#endif
-
-			param[i] = token;
-			token = strtok(NULL, DELIMITERS);
-		}
-		param[i] = 0;
-		#ifdef DEBUG
-			print_debug(shell, param);
-			fprintf(stderr, "param[0] is at %p\n", param[0]);
-		#endif
-		// empty lines = ignore
-		if(!param[0]) continue;
-
-		// Reset errors
-		errno = 0;
-
-		if (strcmp(param[0], "exit") == 0 || strcmp(param[0], "quit") == 0) {
-			break;
-		} else if (strcmp(param[0], "history") == 0 || strcmp(param[0], "h") == 0) {
-			print_error(shell, param[0], 2);
-		} else if (strcmp(param[0], "derp") == 0) {
-			print_error(shell, param[0], 0);
-		} else {
-			// lrn 2 run programs!!! stupid shell
-			// TODO: safefork()
-			// TODO: child searches path, checks access() and executes, then DIES!!!
-			// TODO: parent calls wait(&status) to wait for child to DIEEEE
-			// if safefork returns -1: error and continue to top
-			// returns > 0: we are parent, call wait() and then return to top
-			// returns 0: we are child, do our stuff and exit (DO NOT LET IT AVOID EXIT)
-			//
-			//
-			// TODO in backgrounding:
-			// * every time we background a process, we have to throw that process ID into a list
-			// * after every line read, we call waitpid with WNOHANG. for each pid that has returned we can remove it from the list
-			// * at the end of the program, wait on all childs left (blocking)
-			pid_t pid = safefork();
-			if(pid < 0){
-				// error: Seems we have hit our process limit
-			} else if(pid == 0){
-				// I'm a child!! yey
-				path = find_path(param[0]);
-				
-				// in case execve fails to execute, we have to exit ourselves
-				if (path == NULL) {
-					print_error(shell, param[0], 1);
-					exit(127);
-				} else {
-					job = execve(path, param, __environ);
-					exit(job); 
-				}
-			} else {
-				int status;
-				// advanced edition, waits on specific pid (in case any backgrounded process returns first)
+		else break;
+	}
 #ifdef DEBUG
-				fprintf(stderr, "Child forked with pid %i\n", pid);
+	fprintf(stderr, "Background? %s\n", background ? "yes" : "no");
 #endif
+
+	// Get the first token (command)
+	token = strtok(line, DELIMITERS);
+
+	// Iterate throgh the rest (parameters)
+	for (i = 0; token != NULL && i < 20;i++) {
+#ifdef DEBUG
+			printf("%s --> param[%d]\n", token, i);
+#endif
+
+		param[i] = token;
+		token = strtok(NULL, DELIMITERS);
+	}
+	param[i] = 0;
+#ifdef DEBUG
+	print_debug(shell, param);
+	fprintf(stderr, "param[0] is at %p\n", param[0]);
+#endif
+	// empty lines = ignore
+	if(!param[0]) return;
+
+	// Reset errors
+	errno = 0;
+
+	if (strcmp(param[0], "exit") == 0 || strcmp(param[0], "quit") == 0) {
+		run = false;
+		return;
+	} else if (strcmp(param[0], "history") == 0 || strcmp(param[0], "h") == 0) {
+		print_error(shell, param[0], 2);
+	} else if (strcmp(param[0], "derp") == 0) {
+		print_error(shell, param[0], 0);
+	} else {
+		// lrn 2 run programs!!! stupid shell
+		// TODO: safefork()
+		// TODO: child searches path, checks access() and executes, then DIES!!!
+		// TODO: parent calls wait(&status) to wait for child to DIEEEE
+		// if safefork returns -1: error and continue to top
+		// returns > 0: we are parent, call wait() and then return to top
+		// returns 0: we are child, do our stuff and exit (DO NOT LET IT AVOID EXIT)
+		//
+		//
+		// TODO in backgrounding:
+		// * every time we background a process, we have to throw that process ID into a list
+		// * after every line read, we call waitpid with WNOHANG. for each pid that has returned we can remove it from the list
+		// * at the end of the program, wait on all childs left (blocking)
+		pid_t pid = safefork();
+		if(pid < 0){
+			// error: Seems we have hit our process limit
+		} else if(pid == 0){
+			// I'm a child!! yey
+			path = find_path(param[0]);
+			
+			// in case execve fails to execute, we have to exit ourselves
+			if (path == NULL) {
+				print_error(shell, param[0], 1);
+				exit(127);
+			} else {
+				job = execve(path, param, __environ);
+				exit(job); 
+			}
+		} else {
+			int status;
+			// advanced edition, waits on specific pid (in case any backgrounded process returns first)
+#ifdef DEBUG
+			fprintf(stderr, "Child forked with pid %i\n", pid);
+#endif
+			if(!background){
 				waitpid(pid, &status, 0);
 #ifdef DEBUG
 				fprintf(stderr, "Child exited with code %i\n", WEXITSTATUS(status));
@@ -192,8 +184,30 @@ int main(void) {
 				// we are parent, int status; wait(&status); until child is done.. if background, just skip this (when you feel like getting better grades, read up on waitpid()
 			}
 		}
-		if (errno) print_error(shell, param[0], 1);
 	}
+	if (errno) print_error(shell, param[0], 1);
+}
+
+int main(void) {
+	#ifdef DEBUG
+		printf("Running in debug mode\n");
+	#endif
+
+	char line[MAX_LENGTH];
+	bool run = true;
+
+	// Program main loop
+	while (!feof(stdin) && run == true) {
+		prompt();
+		if (!fgets(line, MAX_LENGTH, stdin)) break;
+		
+		// Zombie apocalypse deterrent
+		while (waitpid(-1, NULL, WNOHANG) > 0);
+
+		runc(line, run);
+	}
+	// Zombie apocalypse deterrent
+	while (waitpid(-1, NULL, WNOHANG) > 0);
 
 	return 0;
 }
