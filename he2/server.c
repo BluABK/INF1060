4ff2b82d1d38cd807bf324d6c01bf8545da31a9b
BluABK <abk@blucoders.net>
2014-11-21 17:25:46 +0100

    Massive overhaul, and select() keeps returning -1 for some reason..

--- he2/server.c
+++ he2/server.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h> 	// memset
 #include <unistd.h> 	// close
+#include <time.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>		// htonl, htons, ntohl, ntohs ?
 #include <arpa/inet.h>		// htonl, htons, ntohl, ntohs
+#include <sys/select.h>		// fd_set
 
 int main(int argc, char* argv[]) {
 	
@@ -21,36 +23,92 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 	
-	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	// Declarations
+	struct sockaddr_in serveraddr;
+	struct sockaddr_in clientaddr;
+	int clientaddrlen, i, retv;
+	int request_sd, sd[2], numsocks, maxsocks;
+	char buf[13];
+	fd_set fds, readfds;
+	struct timeval timeout;
 
-	if (fd == -1) {
-		perror("socket");
-		return -1;
-	}
-
-	struct sockaddr_in binfo;
-	memset(&binfo, 0, sizeof(binfo));
-
-	binfo.sin_family = AF_INET;
-	binfo.sin_port = htons(port);		// 16 bit host to network short
-
-	binfo.sin_addr.s_addr = INADDR_ANY;	// Listen on any IP
+	numsocks = 0, maxsocks = 2;
+	timeout.tv_sec = 20;
+	timeout.tv_usec = 0;
+	// Create Socket
+	request_sd = socket(AF_INET, SOCK_STREAM, 0);
 	
-	// Bind socket
-	int retv = bind(fd, (struct sockaddr*)&binfo, sizeof(binfo));
+	// Fill in the address structure
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(port);		// 16 bit host to network short
+	serveraddr.sin_addr.s_addr = INADDR_ANY;	// Listen on any IP
 
+	// Bind address to socket
+	retv = bind(request_sd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
 	if(retv != 0) {
-		perror("bind");
+		perror("bind()");
 		return -2;
 	}
-
-	retv = listen(fd, 5);
+	// Activate the connect request queue
+	retv = listen(request_sd, SOMAXCONN);
 	printf("Listening on port %i\n", port);
 	if (retv != 0) {
-		perror("listen");
+		perror("listen()");
 		return -3;
 	}
 
+	// Initialise fd set
+	FD_ZERO(&fds);
+	FD_SET(request_sd, &fds);
+
+	while (1) {
+		readfds = fds;
+		retv = select(FD_SETSIZE, &readfds, NULL, NULL, &timeout);
+
+		// Something went wrong
+		if (retv == -1) {
+			perror("select()");
+			printf("select() returned %i\n", retv);
+			return -4;
+		}
+		// nothing happened; select continued
+		if (retv == 0) {
+			printf("Connection timeout\n");
+			
+			for (i = 0; i < numsocks; i++) {
+				// Send a response
+				write(sd[i], "Server ACK", 11);
+				// Close sockets
+				close(sd[i]);
+				FD_CLR(sd[i], &fds);
+			}
+			return 0;
+		}
+		
+		for (i = 0; i < FD_SETSIZE; i++) {
+			if (FD_ISSET(i, &readfds)) {
+				if (i == request_sd) {
+					// New connection request
+					if (numsocks < maxsocks) {
+						sd[numsocks] = accept(request_sd, (struct sockaddr *)&clientaddr, (socklen_t *)&clientaddrlen);
+						FD_SET(sd[numsocks], &fds);
+						numsocks++;
+					} else {
+						printf("Ran out of socket space.\n");
+						return -5;
+					}
+				} else {
+					// Data arrived on an existing socket - clientstuff
+					read(i, buf, 12);
+					buf[12] = 0;
+					printf("From socket. %d: %s\n", i, buf);
+				}
+			}
+		}
+	close(request_sd);
+
+	/*
 	struct sockaddr_in cinfo;
 	memset(&cinfo, 0, sizeof(cinfo));
 	socklen_t info_len = sizeof(cinfo);
@@ -61,11 +119,11 @@ int main(int argc, char* argv[]) {
 	}
 	char cbuf[16]; // TODO: Possibly too ambitious size
 	const char *tha_client = inet_ntop(AF_INET, &cinfo.sin_addr.s_addr, cbuf, info_len);
-/*	if (retv !=1) {
+	if (retv !=1) {
 		perror("inet_ntop");
 		return -5;
 	}*/
-
+/*
 	printf("Client '%s' connected on remote port: %d\n", tha_client, ntohs(cinfo.sin_port));
 	
 	while (1) {
@@ -76,7 +134,7 @@ int main(int argc, char* argv[]) {
 			buf[rd] = 0;
 			printf("RECV: %zd bytes from client on fd %d: %s\n", rd, client_fd, buf);
 			
-			if (buf == "DISCONNECT\n") {
+			if (!strcmp(buf, "DISCONNECT\n")) {
 				printf("Client '%s:%d' disconnected from server\n", tha_client, ntohs(cinfo.sin_port));
 			}
 		} else {
@@ -88,14 +146,12 @@ int main(int argc, char* argv[]) {
 		if (sent != -1) {
 			printf("SEND: %zd bytes to client on fd %d: %s\n", sent, client_fd, testword);
 		} else {
-			printf("SENDFAIL: to client on fd %d: %s\n", sent, client_fd, testword);
+			printf("SENDFAIL: %li bytes to client on fd %d: %s\n", sent, client_fd, testword);
 		}
-
+		*/
 	}
 
-	close(client_fd);
-	close(fd);
-
-
+//	close(client_fd);
+//	close(fd);
 	return 0;
 }
